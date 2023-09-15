0b56993078b5106e83dd06ba42ce2caba8b5ed53
Bruno Henrique <brunohenriquex00@gmail.com>
2023-09-14 23:20:23 -0300

    Criando metodo add

--- src/ListaEncadeada.java
+++ src/ListaEncadeada.java
@@ -9,6 +9,19 @@ public class ListaEncadeada<T> {
         return this.refEntrada == null;
     }
 
+    public void add(T conteudo){
+        No<T> novoNo = new No<>(conteudo);
+        if(this.isEmpty()) {
+            this.refEntrada = novoNo;
+            return;
+        }
+        No<T> noAux = refEntrada;
+        for (int i = 0; i < this.size()-1; i++) {
+            noAux = noAux.getProxNo();
+        }
+        noAux.setProxNo(novoNo);
+    }
+
     public int size() {
         int tamLista = 0;
 
