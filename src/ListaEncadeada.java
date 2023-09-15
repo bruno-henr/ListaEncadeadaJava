b7683d61cfcaac3aadffc197b9a480fb6c38474d
Bruno Henrique <brunohenriquex00@gmail.com>
2023-09-15 10:31:00 -0300

    implementando codigo para funcao de validaIndice, get, getNo e remove

--- src/ListaEncadeada.java
+++ src/ListaEncadeada.java
@@ -22,6 +22,39 @@ public class ListaEncadeada<T> {
         noAux.setProxNo(novoNo);
     }
 
+    private void validaIndice(int index) {
+        if(index >= this.size()) {
+            throw new IndexOutOfBoundsException("Indice nao existente");
+        }
+    }
+
+    public T remove(int index) {
+        No<T> noPivo = this.getNo(index);
+        if(index == 0) {
+            this.refEntrada = noPivo.getProxNo();
+            return noPivo.getConteudo();
+        }
+        No<T> noAnterior = getNo(index - 1);
+        noAnterior.setProxNo(noPivo.getProxNo());
+        return noPivo.getConteudo();
+    }
+
+    public T get(int index) {
+        return getNo(index).getConteudo();
+    }
+    private No<T> getNo(int index) {
+        validaIndice(index);
+
+        No<T> aux = this.refEntrada;
+        No<T> noRetorno = null;
+        for(int i = 0; i < this.size()-1; i++) {
+            noRetorno = aux;
+            aux = aux.getProxNo();
+        }
+        return noRetorno;
+    }
+
+
     public int size() {
         int tamLista = 0;
 
